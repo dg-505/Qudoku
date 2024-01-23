0fe07da70582f06bffa71141b1b4bb1417696bdd
dg-505 <dg-505@web.de>
2024-01-23 21:50:16 +0100

    Instead of 'DataDir', use  'HomeDir' in config file.\nSpecify 'data' and 'export' subdirs when needed

--- Code/src/gui/SolvedGUI.cpp
+++ Code/src/gui/SolvedGUI.cpp
@@ -82,7 +82,7 @@ namespace sudoku
         {
             _name = "sudoku";
         }
-        pixmap.save(_settings->value(QStringLiteral("DIRS/DataDir"), QVariant()).toString() + QStringLiteral("/../export/") + QString::fromStdString(_name) + QStringLiteral("-solution-") + QDateTime::currentDateTime().toString("yyyyMMdd_hhmmss") + QStringLiteral(".png"), "png", 0);
+        pixmap.save(_settings->value(QStringLiteral("DIRS/HomeDir"), QVariant()).toString() + QStringLiteral("/export/") + QString::fromStdString(_name) + QStringLiteral("-solution-") + QDateTime::currentDateTime().toString("yyyyMMdd_hhmmss") + QStringLiteral(".png"), "png", 0);
     }
 
     auto SolvedGUI::exportPDF(QWidget* parent) -> void
@@ -94,7 +94,7 @@ namespace sudoku
         }
 
         // Create output pdf
-        QPdfWriter pdfWriter(_settings->value(QStringLiteral("DIRS/DataDir"), QVariant()).toString() + QStringLiteral("/../export/") + QString::fromStdString(_name) + QStringLiteral("-solution-") + QDateTime::currentDateTime().toString("yyyyMMdd_hhmmss") + QStringLiteral(".pdf"));
+        QPdfWriter pdfWriter(_settings->value(QStringLiteral("DIRS/HomeDir"), QVariant()).toString() + QStringLiteral("/export/") + QString::fromStdString(_name) + QStringLiteral("-solution-") + QDateTime::currentDateTime().toString("yyyyMMdd_hhmmss") + QStringLiteral(".pdf"));
         pdfWriter.setPageSize(QPageSize(2 * parent->size(), QPageSize::Point, QString(), QPageSize::FuzzyMatch));
         pdfWriter.setPageMargins(QMarginsF(0, 0, 0, 0));
 
--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -9,18 +9,19 @@ auto main(int argc, char* argv[]) -> int
     Q_INIT_RESOURCE(resources);
 
 #ifndef _WIN32
-    // if not existent, create config file and example in /home/<USER>/.qudoku
-    auto cfgDir = QDir::homePath() + QStringLiteral("/.qudoku");
-    if (!QDir(cfgDir).exists())
+    // if not existent, create config file in /home/<USER>/.qudoku
+    auto homeDir = QDir::homePath() + QStringLiteral("/.qudoku");
+    QFile iniFile(homeDir + QStringLiteral("/Qudoku.ini"));
+    if (!iniFile.exists())
     {
-        QDir(QString()).mkdir(cfgDir);
-        QFile iniFile(cfgDir + QStringLiteral("/Qudoku.ini"));
+        QDir(QString()).mkdir(homeDir);
         if (iniFile.open(QFile::WriteOnly | QFile::Truncate))
         {
             QTextStream ini(&iniFile);
-            ini << "[DIRS]\nDataDir=" << cfgDir << "/data\n\n[I18N]\nlang=en_US\n";
+            ini << "[DIRS]\nHomeDir=" << homeDir << "\n\n[I18N]\nlang=en_US\n";
         }
-        auto dataDir = cfgDir + QStringLiteral("/data");
+        iniFile.close();
+        auto dataDir = homeDir + QStringLiteral("/data");
         QDir(QString()).mkdir(dataDir);
         QFile exampleFile(dataDir + QStringLiteral("/example.txt"));
         if (exampleFile.open(QFile::WriteOnly | QFile::Truncate))
@@ -28,7 +29,7 @@ auto main(int argc, char* argv[]) -> int
             QTextStream example(&exampleFile);
             example << ".3..........195.....8....6.8...6....4..8....1....2.....6....28....419..5.......7.";
         }
-        QDir(QString()).mkdir(cfgDir + QStringLiteral("/export"));
+        QDir(QString()).mkdir(homeDir + QStringLiteral("/export"));
     }
 #endif
 
